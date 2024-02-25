f604a82e993a76385f24bbb1726c4a647b771de4
Avichaurasia <avichaurasia1994@gmail.com>
2024-02-25 12:02:40 +0100

    Updated Portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,12 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
 
 (function($) {
 
